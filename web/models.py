ae02a48e4aec97144f2ab147410bcce9f6c92825
Martha Kumwenda <marthakumwenda93@gmail.com>
2018-02-02 12:44:49 +0200

    created a model local added text to template.html

--- web/models.py
+++ web/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Local(models.Model):
+    destination = models.CharField(max_length=30, null=True)
+    price = models.IntegerField(max_length=10)
